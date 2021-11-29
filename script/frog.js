a27e646e1999d483c0f3ddad32b32ca017df3356
David Pallarés <serallapdivad@gmail.com>
2021-11-29 22:11:03 +0000

    edge limits, new frog when reach nest, styles

--- script/frog.js
+++ script/frog.js
@@ -1,36 +1,55 @@
 function Frog() {
-  let self = this;
   this.squashed = false;
   this.nestReached = false;
-  this.start = document.querySelector("#start");
-  this.frog = document.createElement("div");
-  this.frog.setAttribute("id", "frog");
-  this.start.appendChild(this.frog);
-  //this.frog = document.querySelector("#frog");
-  this.pos = {
-    left: 370,
-    top: 7,
-  };
+  this.start = null;
+  this.frog = null;
+  // this.frog.setAttribute("id", "frog");
+  // this.start.appendChild(this.frog);
+  this.pos = null;
   this.direction = null;
 
+  this.create = function () {
+    this.squashed = false;
+    this.start = document.querySelector("#start");
+    this.frog = document.createElement("div");
+    this.frog.setAttribute("id", "frog");
+    this.start.appendChild(this.frog);
+    this.pos = {
+      left: 370,
+      top: 7,
+    };
+  };
+
+  this.clear = function () {
+    this.start.removeChild(this.frog);
+  };
+  //set canvas limit
+
   this.moveUp = function () {
-    console.log(this.pos.top)
-    this.pos.top -= 55;
-    this.frog.style.top = `${this.pos.top}px`;
+    if (this.pos.top > -543) {
+      this.pos.top -= 55;
+      this.frog.style.top = `${this.pos.top}px`;
+    }
   };
 
   this.moveDown = function () {
-    this.pos.top += 55;
-    this.frog.style.top = `${this.pos.top}px`;
+    if (this.pos.top < 7) {
+      this.pos.top += 55;
+      this.frog.style.top = `${this.pos.top}px`;
+    }
   };
 
   this.moveLeft = function () {
-    this.pos.left -= 55;
-    this.frog.style.left = `${this.pos.left}px`;
+    if (this.pos.left > 40) {
+      this.pos.left -= 55;
+      this.frog.style.left = `${this.pos.left}px`;
+    }
   };
 
   this.moveRight = function () {
-    this.pos.left += 55;
-    this.frog.style.left = `${this.pos.left}px`;
+    if (this.pos.left < 700) {
+      this.pos.left += 55;
+      this.frog.style.left = `${this.pos.left}px`;
+    }
   };
 }
--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,10 @@
-var frogMovementSound = new Audio("../sfx/jumpSound/jump.flac");
+// var frogMovementSound = new Audio("../sfx/jumpSound/jump.flac");
 //var squashedFrog = new Audio('../sfx/squased.wav')
 
 let posL = 0;
 let posR = 0;
+
+let lives = 2;
 
 // se crea una rana
 const frog = new Frog();
@@ -23,11 +25,18 @@ function startGame() {
   const startGameBox = document.querySelector("#startWindow");
   startGameBox.classList.add("hideStartGame");
 
-  setInterval(function (){
-      winGame();
+  frog.create(); //create a frog
+  setInterval(function () {
+    reachNest();
     if (detectCollision(frog)) {
       // squashedFrog.play()
-      gameOver();
+      if (lives === 0) {
+        gameOver();
+      } else {
+        lives--;
+        frog.clear();
+        frog.create();
+      }
     }
     carLoop();
   }, 50);
@@ -37,33 +46,40 @@ const moveOnceKeyPressed = document.querySelector("#start");
 
 // frog movement
 
-
 window.addEventListener("keydown", function (event) {
   if (event.code === "ArrowUp") {
     if (frog.squashed === false) {
-      frogMovementSound.play();
+      // frogMovementSound.pause();
+      // frogMovementSound.currentTime = 0;
+      // frogMovementSound.play();
       frog.moveUp();
-      this.frog.setAttribute("class", "up");
+      // this.frog.setAttribute("class", "up");
     }
   }
 
   if (event.code === "ArrowDown") {
     if (frog.squashed === false) {
-      frogMovementSound.play();
+      // frogMovementSound.pause();
+      // frogMovementSound.currentTime = 0;
+      // frogMovementSound.play();
       frog.moveDown();
     }
   }
 
   if (event.code === "ArrowLeft") {
     if (frog.squashed === false) {
-      frogMovementSound.play();
+      // frogMovementSound.pause();
+      // frogMovementSound.currentTime = 0;
+      // frogMovementSound.play();
       frog.moveLeft();
     }
   }
 
   if (event.code === "ArrowRight") {
     if (frog.squashed === false) {
-      frogMovementSound.play();
+      // frogMovementSound.pause();
+      // frogMovementSound.currentTime = 0;
+      // frogMovementSound.play();
       frog.moveRight();
     }
   }
@@ -101,16 +117,78 @@ function gameOver() {
   grayScaleGZ.classList.add("filter");
 }
 
-function winGame() { 
-if (frog.pos.top <= -540 && frog.pos.left >= 142 && frog.pos.left <= 187) {
-  console.log('nest0')
-} else if (frog.pos.top <= -540 && frog.pos.left >= 255 && frog.pos.left <= 300) {
-  console.log('nest1')
-} else if (frog.pos.top <= -540 && frog.pos.left >= 365 && frog.pos.left <= 410) {
-  console.log('nest2')
-} else if (frog.pos.top <= -540 && frog.pos.left >= 475 && frog.pos.left <= 520) {
-  console.log('nest3')
-} else if (frog.pos.top <= -540 && frog.pos.left >= 585 && frog.pos.left <= 630) {
-  console.log('nest4')
+const reachedNest = [false, false, false, false, false];
+
+function reachNest() {
+  if (frog.pos.top <= -540 && frog.pos.left >= 142 && frog.pos.left <= 187) {
+    if (reachedNest[0] === false) {
+      reachedNest[0] = true;
+      const addFrogImg = document.querySelector("#nest0");
+      addFrogImg.setAttribute("class", "frog");
+      winGame();
+    }
+    //reset frog
+  } else if (
+    frog.pos.top <= -540 &&
+    frog.pos.left >= 255 &&
+    frog.pos.left <= 300
+  ) {
+    if (reachedNest[1] === false) {
+      reachedNest[1] = true;
+      const addFrogImg = document.querySelector("#nest1");
+      addFrogImg.setAttribute("class", "frog");
+      winGame();
+    }
+  } else if (
+    frog.pos.top <= -540 &&
+    frog.pos.left >= 365 &&
+    frog.pos.left <= 410
+  ) {
+    if (reachedNest[2] === false) {
+      reachedNest[2] = true;
+      const addFrogImg = document.querySelector("#nest2");
+      addFrogImg.setAttribute("class", "frog");
+      winGame();
+    }
+  } else if (
+    frog.pos.top <= -540 &&
+    frog.pos.left >= 475 &&
+    frog.pos.left <= 520
+  ) {
+    if (reachedNest[3] === false) {
+      reachedNest[3] = true;
+      const addFrogImg = document.querySelector("#nest3");
+      addFrogImg.setAttribute("class", "frog");
+      winGame();
+    }
+  } else if (
+    frog.pos.top <= -540 &&
+    frog.pos.left >= 585 &&
+    frog.pos.left <= 630
+  ) {
+    if (reachedNest[4] === false) {
+      reachedNest[4] = true;
+      const addFrogImg = document.querySelector("#nest4");
+      addFrogImg.setAttribute("class", "frog");
+      winGame();
+    }
+  }
 }
+
+function winGame() {
+  if (
+    reachedNest.filter(function (e) {
+      return e;
+    }).length === 5
+  ) {
+    console.log("youWin");
+  } else {
+    frog.clear();
+    frog.create();
+    //carSpeed = carSpeed.map(speed => speed++)
+  }
 }
+
+//set canvas limits
+//lives counter
+//
